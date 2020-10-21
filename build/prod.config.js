69c5d9fa2273d2d7609d7496df7476a9523b9fa1
crazyfdf <343224608@qq.com>
2020-10-21 11:39:50 +0800

    imeal下载页

--- build/prod.config.js
+++ build/prod.config.js
@@ -1,9 +1,13 @@
 // const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const webpackMerge = require('webpack-merge')
 const baseConfig = require('./base.config')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = webpackMerge.merge(baseConfig, {
   plugins: [
     // new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      favicon: './src/img/imealLogo.png'
+  })
   ]
 })
